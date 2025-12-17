2cff7b7638c4ea5da1158670013dc9307acc48fd
zweelie <115572065+zweelie@users.noreply.github.com>
2025-12-17 02:45:10 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import ExitIntentPopup from '@/components/ExitIntentPopup'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'Beyond Ticket Defence - We Deliver Peace of Mind',
@@ -19,6 +20,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+
+{/* <Script src="https://www.chatbase.co/embed.min.js" id="z2c2HSfKnCTh5J4650V0I"></Script> */}
+<Script src="https://zweelie.ngrok.app/conversation-widget-iframe/embed.js" id="cbb1154c-8594-4082-afe2-3d54c974292e"></Script>
+
       <body>
         {children}
         <ExitIntentPopup />
